a6dd89f8a81c1dafe25e1fa40a1597d490f2ef9a
Linus Widströmer <linus.widstromer@it.su.se>
2010-11-08 15:06:37 +0100

    Use an index for for looking up nodes and relationships so that they can be reused

--- src/main/java/se/su/it/pcap2neo/neo/impl/NeoStoreImpl.java
+++ src/main/java/se/su/it/pcap2neo/neo/impl/NeoStoreImpl.java
@@ -1,33 +1,41 @@
 package se.su.it.pcap2neo.neo.impl;
 
 
+import org.apache.log4j.Logger;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.index.IndexService;
-import org.neo4j.index.lucene.LuceneIndexService;
+import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.index.IndexHits;
+import org.neo4j.graphdb.index.RelationshipIndex;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
 import org.springframework.stereotype.Component;
-
 import se.su.it.pcap2neo.neo.NeoStore;
-
 
 @Component
 public class NeoStoreImpl implements NeoStore {
 
+	
+	private static final String REF_PROP = "refcnt";
+	
+	private Logger logger = Logger.getLogger(NeoStoreImpl.class);
+	
 	public enum connTypes implements RelationshipType
 	{
 	    TCP, UDP
 	}
 
 	private GraphDatabaseService graphDb; 
-	private IndexService index;
+	private Index<Node> index;
+	private RelationshipIndex relIndex;
 	
 	public NeoStoreImpl() {
+		logger.info("Setting up NeoStoreImpl");
 		graphDb = new EmbeddedGraphDatabase( "/opt/neodb/pcap4neo" );
-		index = new LuceneIndexService( graphDb );
+		index = graphDb.index().forNodes( "host" );
+		relIndex = graphDb.index().forRelationships( "conn" );
 	}
 	
 	
@@ -40,24 +48,10 @@ public class NeoStoreImpl implements NeoStore {
 		Transaction tx = graphDb.beginTx();
 		
 		try {
-
-			// Fetch, or create, the source node
-			Node src = index.getSingleNode( "addr", from);
-			if( src == null) {
-				src = graphDb.createNode();
-				src.setProperty( "addr", from );
-			}
-		
-			// Fetch, or create, the destination node
-			Node dst = index.getSingleNode( "addr", to);
-			if( dst == null) {
-				dst = graphDb.createNode();
-				dst.setProperty( "addr", to );
-			}
-		
-			// Create the relation
-			Relationship syn = src.createRelationshipTo( dst, connTypes.TCP );
-			syn.setProperty( "port", port );
+			Node src = getOrCreateNode("addr", from);
+			Node dst = getOrCreateNode("addr", to);
+			getOrCreateRelationship("port", port.toString(), src, dst);
+			tx.success();
 		}
 		finally {
 			tx.finish();
@@ -67,7 +61,59 @@ public class NeoStoreImpl implements NeoStore {
 		
 	}
 	
+	/**
+	 * Create or fetch a node by attribute
+	 * @param key Primary key name
+	 * @param value Primary key value
+	 * @return The Node created/found
+	 */
+	private Node getOrCreateNode( String key, String value )
+	{
+		IndexHits<Node> res = index.get( key, value);
+		Node node = null;
+		if ( !res.hasNext() ) 
+		{
+			logger.info("No node found for " + key + "=" + value + " - creating it.");
+			node = graphDb.createNode();
+			node.setProperty( key, value);
+			index.add( node, key, value);
+		} else {
+			node = res.getSingle();
+			logger.info("Returning existing node for " + key + "=" + value + " - reusing it.");
+			res.close();
+		}
+		return node;
+	}
+
 	
-	
+	private Relationship getOrCreateRelationship( String key, String value, Node from, Node to )
+	{
+		Relationship rel = null;
+		IndexHits<Relationship> res = null;
+		try {
+			res = relIndex.query( key, value, from, to);
+		} catch(NullPointerException npe) {
+			logger.warn("Got a NPE when searching index, this is not too good.");
+		}
+
+		if ( res==null || !res.hasNext() ) 
+		{
+			logger.info("No relationship found for " + key + "=" + value + " for " + from + "=>" + to + " - creating it.");
+			rel = from.createRelationshipTo( to, connTypes.TCP );
+			rel.setProperty( key, value );
+			rel.setProperty( REF_PROP, 1 );
+			relIndex.add( rel, key, value);
+		} else {
+			logger.info("Returning existing relationship for " + key + "=" + value + " - reusing it.");
+			rel = res.getSingle();
+			res.close();
+			rel.setProperty( key, (Integer)rel.getProperty(REF_PROP) + 1);
+		}
+		return rel
+		;
+	}
+
+
+
 	
 }
